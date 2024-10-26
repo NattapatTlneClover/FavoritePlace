dee67497f4e415c4b8c2c1dff8082672236f896c
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-10-26 17:56:53 +0700

    front-end back-end service fetching data sending data backend sending delete request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
+import { ErrorService } from './shared/error.service';
+import { ErrorModalComponent } from "./shared/modal/error-modal/error-modal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [AvailablePlacesComponent, UserPlacesComponent, ErrorModalComponent],
 })
 export class AppComponent {
-  title = 'favorite-place';
+  private errorService = inject(ErrorService);
+
+  error = this.errorService.error;
 }
