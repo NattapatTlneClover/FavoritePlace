884a7f8d056ef6f18a0fad6d1167bed888ff0a3b
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-10-26 18:09:38 +0700

    HTTP Interceptors

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -26,9 +26,6 @@ export class UserPlacesComponent implements OnInit {
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      // next: (places) => {
-      //   this.places.set(places);
-      // },
       error: (error: Error) => {
         this.error.set(error.message);
       },
